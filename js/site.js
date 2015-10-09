97867206e152a0a51ce347358feabf10e5a3d4ab
SJohnson <simonbjohnson@gmail.com>
2015-10-09 17:18:39 +0100

    Switched to ISO 3

--- js/site.js
+++ js/site.js
@@ -1,11 +1,11 @@
 function generateDashboard(data,geom){
-    var map = new lg.map('#map').geojson(geom).joinAttr('CNTRY_NAME').zoom(3).center([50,20]);
+    var map = new lg.map('#map').geojson(geom).joinAttr('Iso_Code').zoom(3).center([50,20]);
     var grid = new lg.grid('#grid')
         .data(data)
         .width($('#grid').width())
         .height(1500)
         .nameAttr('Country')
-        .joinAttr('JoinAttr')
+        .joinAttr('ISO 3 code')
         .hWhiteSpace(10)
         .vWhiteSpace(10)
         .valuesList(['GDP (millions $ 2014)','Population','Refugee Population 2014','Refugee Population 2014 (%)','National Staff','Volunteers',' RC Income (Swiss Franks)'])
@@ -41,7 +41,6 @@ function hxlProxyToJSON(input,headers){
 }
 
 function stickydiv(){
-    console.log('cehc')
     var window_top = $(window).scrollTop();
     var div_top = $('#sticky-anchor').offset().top;
     if (window_top > div_top){
@@ -60,7 +59,7 @@ $(window).scroll(function(){
 
 var dataCall = $.ajax({ 
     type: 'GET', 
-    url: 'http://proxy.hxlstandard.org/data.json?filter_count=7&url=https%3A//docs.google.com/spreadsheets/d/1m3XUjtbLWKEUpUKu4ic16ruDeNj7yuZG2_1Y8U-tvs0/pub%3Fgid%3D0%26single%3Dtrue%26output%3Dcsv&format=html&filter01=&filter02=&filter03=&filter04=&filter05=&filter06=&filter07=&force=1', 
+    url: 'http://proxy.hxlstandard.org/data.json?filter01=replace-map&replace-map-url01=https%3A//docs.google.com/spreadsheets/d/12TdWAO9BmavBkGEM-7hPV7IMjN_EOJY_2iGnW_ezjuk/pubhtml%3Fgid%3D493036357%26single%3Dtrue&filter02=merge&merge-url02=https%3A//docs.google.com/spreadsheets/d/12TdWAO9BmavBkGEM-7hPV7IMjN_EOJY_2iGnW_ezjuk/pub%3Fgid%3D0%26single%3Dtrue%26output%3Dcsv&merge-tags02=%23country%2Bcode&merge-keys02=%23country-code&filter03=&filter04=&filter05=&filter06=&filter07=&url=https%3A//docs.google.com/spreadsheets/d/1m3XUjtbLWKEUpUKu4ic16ruDeNj7yuZG2_1Y8U-tvs0/pub%3Fgid%3D0%26single%3Dtrue%26output%3Dcsv&force=1', 
     dataType: 'json',
 });
 
@@ -68,14 +67,16 @@ var dataCall = $.ajax({
 
 var geomCall = $.ajax({ 
     type: 'GET', 
-    url: 'data/geom.geojson', 
+    url: 'data/geom.json', 
     dataType: 'json',
 });
 
 //when both ready construct dashboard
 
 $.when(dataCall, geomCall).then(function(dataArgs, geomArgs){
-    geom = geomArgs[0];
+    var geom = topojson.feature(geomArgs[0],geomArgs[0].objects.geom);
+    console.log(geom);
     var data = hxlProxyToJSON(dataArgs[0],true);
+    console.log(data);
     generateDashboard(data,geom);
 });
